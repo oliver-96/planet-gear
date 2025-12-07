282a5f085e371ff0b9f2490de1973f22a1929aef
Oliver Atkinson <oliver.atkinson96@gmail.com>
2025-12-07 17:58:53 +0000

    update

--- run.py
+++ run.py
@@ -58,7 +58,7 @@ k_ax_slider = plt.axes([0.25, 0.1, 0.5, 0.03])  # [left, bottom, width, height]
 a_ax_slider = plt.axes([0.25, 0.05, 0.5, 0.03])  # [left, bottom, width, height]
 speed_ax_slider = plt.axes([0.25, 0.01, 0.5, 0.03])  # [left, bottom, width, height]
 
-k_slider = Slider(k_ax_slider, 'k = R/r', 1, 10, valinit=k_initial, valstep=0.1)
+k_slider = Slider(k_ax_slider, 'k = R/r', 0, 10, valinit=k_initial, valstep=0.1)
 a_slider = Slider(a_ax_slider, 'a', 0, 10, valinit=a_initial, valstep=0.1)
 speed_slider = Slider(speed_ax_slider, 'Speed', 0, 1, valinit=0.5, valstep=0.05)
 
@@ -105,7 +105,6 @@ def update(val):
 # --- Connect widgets ---
 k_slider.on_changed(update)
 a_slider.on_changed(update)
-# speed_slider.on_changed(update)
 mode_radio.on_clicked(update)
 
 # --- Animation update ---
